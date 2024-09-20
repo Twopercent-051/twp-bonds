7a8f55184287d0f7cd1ea63cf714ed9335b0e61f
twopercent <twopercent051@zohomail.eu>
2024-09-20 13:25:15 +0400

    a

--- web_app/router.py
+++ web_app/router.py
@@ -14,7 +14,7 @@ async def get_bonds(request: Request):
     bonds = await MoexAPI.get_bonds_profiles(sql_bonds=sql_bonds)
     total_amount = sum(bond.amount for bond in bonds)
     total_nominal = sum(bond.nominal for bond in bonds)
-    total_price = sum(bond.price + bond.nkd for bond in bonds)
+    total_price = sum(bond.price for bond in bonds)
     return templates.TemplateResponse("bonds_table.html", {
         "request": request,
         "bonds": bonds,
