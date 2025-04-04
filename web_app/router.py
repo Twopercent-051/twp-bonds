95734c9e39c246ee30bf8a0c5808c7cc7ecc69cd
twopercent <twopercent051@zohomail.eu>
2025-04-04 17:26:21 +0400

    fix

--- web_app/router.py
+++ web_app/router.py
@@ -16,9 +16,9 @@ async def get_bonds(request: Request):
     total_nominal = sum(bond.nominal for bond in bonds)
     total_price = round(number=sum(bond.price for bond in bonds), ndigits=2)
     balances = await MoneyBalanceDAO.get_many()
-    total = sum(balance.amount for balance in balances)
+    balance = sum(balance.amount for balance in balances)
     total_deposits = sum(balance.amount for balance in balances if balance.description == "deposit")
-    difference = total + total_price - total_deposits
+    difference = balance + total_price - total_deposits / 100
     return templates.TemplateResponse(
         "bonds_table.html",
         {
@@ -27,7 +27,7 @@ async def get_bonds(request: Request):
             "total_amount": total_amount,
             "total_price": total_price,
             "total_nominal": total_nominal,
-            "current_balance": round(number=total / 100, ndigits=2),
+            "current_balance": round(number=balance / 100, ndigits=2),
             "difference": round(number=difference, ndigits=2),
         },
     )
